5457aaee3e2335ade12e916992578d32174d37fc
Henrik Vik <henkvik@gmail.com>
2016-11-09 13:53:49 +0100

    all shapu

--- Scene.cpp
+++ Scene.cpp
@@ -36,9 +36,9 @@ void buildScene()
 		shapes.push_back(dynamic_cast<Shape*>(new Sphere(Vec(400, 400, 300), 200, Color(150, 25, 0))));
 
 		// two triangles 
-		//shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 350, 100, 200 }, { 300, 100, 200 }, { 400, 700,  30 }, { 200,   0,   0 })));
-		//shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 100, 300,   0 }, { 150, 300,   0 }, { 100, 100,   0 }, {   0,   0, 255 })));
-		//shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 150, 300,   0 }, { 150, 100,   0 }, { 100, 100,   0 }, {   0, 255, 255 })));
+		shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 350, 100, 200 }, { 300, 100, 200 }, { 400, 700,  30 }, { 200,   0,   0 })));
+		shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 100, 300,   0 }, { 150, 300,   0 }, { 100, 100,   0 }, {   0,   0, 255 })));
+		shapes.push_back(dynamic_cast<Shape*>(new Triangle({ 150, 300,   0 }, { 150, 100,   0 }, { 100, 100,   0 }, {   0, 255, 255 })));
 
 
 		float size = 20;
@@ -48,7 +48,7 @@ void buildScene()
 			Vec off{ size * 2 * i, 700.0, 0.0 };
 			p0.x = cosf(TORAD(i * (90 / 20.0f))) * size; p0.z = -sinf(TORAD(i * (90 / 20.0f))) * size;
 			p1.x = -p0.x; p1.z = -p0.z;
-			//shapes.push_back(dynamic_cast<Shape*>(new Triangle(p0 + off, p1 + off, p2 + off, { 255,255,0 })));
+			shapes.push_back(dynamic_cast<Shape*>(new Triangle(p0 + off, p1 + off, p2 + off, { 255,255,0 })));
 		}
 
 		// one OBB touching the sphere on the side
